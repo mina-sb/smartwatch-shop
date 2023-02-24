44046a4f617a2e498cbc16adeaed51752c069e3b
mina <sadeghbayan.mina@gmail.com>
2023-02-24 19:00:40 +0330

    responsive menu

--- scripts.js
+++ scripts.js
@@ -9,3 +9,16 @@ darkmode.onclick = () => {
     document.body.classList.remove("color");
   }
 };
+
+let menuBtn = document.querySelector("#menu-icon");
+let menu = document.querySelector(".nav-menu");
+
+menuBtn.onclick = () => {
+  if (menuBtn.classList.contains("bx-menu")) {
+    menuBtn.classList.replace("bx-menu", "bx-x");
+    menu.classList.replace("close", "open");
+  } else {
+    menuBtn.classList.replace("bx-x", "bx-menu");
+    menu.classList.replace("open", "close");
+  }
+};
